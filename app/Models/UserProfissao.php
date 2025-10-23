a16d4aa1bf23ad276acafd38fdd764e1f5d91aa5
Thiago <thiagosaboia777@icloud.com>
2025-10-23 20:45:24 -0300

    Finalização da parte de 'UserProfissao'

--- app/Models/UserProfissao.php
+++ app/Models/UserProfissao.php
@@ -1,13 +1,15 @@
 <?php
 namespace App\Models;
 class UserProfissao {
+
+    public ?int $id;
     public string $codigo;
     public string $descricao_profissao;
     
-    public function __construct(?string $codigo,string $descricao_profissao){
+    public function __construct(?int $id,string $codigo,string $descricao_profissao){
      $this->codigo=$codigo; $this->descricao_profissao=$descricao_profissao;
     }
     public static function fromArray(array $d): self {
-        return new self($d['codigo'],$d['descricao_profissao']);
+        return new self($d['id']??null,$d['codigo'],$d['descricao_profissao']);
     }
 }
